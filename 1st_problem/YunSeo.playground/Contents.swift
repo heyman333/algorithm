1c631694ea1d7e4e045019fbd1e61b4dee8957ed
KimYunseo <lihit98@gmail.com>
2018-01-26 00:57:36 +0900

    문제를 잘못풀어 다시 올림
    문제를 잘못풀어 다시 올림

--- 1st_problem/YunSeo.playground/Contents.swift
+++ 1st_problem/YunSeo.playground/Contents.swift
@@ -1,6 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+
 struct PointInfoStruct {
     var sign: Bool
     var fabsValue: Double
@@ -15,17 +16,24 @@ struct PointInfoStruct {
     }
 }
 struct ResultStruct {
-    var centerPointString: String
-    var anotherPointString: String
+    var centerPoint: PointInfoStruct
+    var anotherPoint: PointInfoStruct
     var distance: Double
 }
 
-var centerPoint = PointInfoStruct.init(-1)
-var pointA = PointInfoStruct.init(3.5)
-var pointB = PointInfoStruct.init(-4.2)
-var pointC = PointInfoStruct.init(7)
-var pointArray: [PointInfoStruct] = [pointA, pointB, pointC]
+extension Double {
+    func roundToSix() -> Double {
+        return Double(String(format: "%.6f", self))!
+    }
+}
 
+func makePoints(_ points: [Double]) -> [PointInfoStruct] {
+    var results: [PointInfoStruct] = [PointInfoStruct]()
+    for point in points {
+        results.append(PointInfoStruct.init(point))
+    }
+    return results
+}
 func distancePointToPoint(_ startPoint: PointInfoStruct, _ targetPoint: PointInfoStruct) -> Double {
     if startPoint.sign == true && targetPoint.sign == true {
         var highValue: Double = startPoint.fabsValue
@@ -34,14 +42,14 @@ func distancePointToPoint(_ startPoint: PointInfoStruct, _ targetPoint: PointInf
             highValue = targetPoint.fabsValue
             lowValue = startPoint.fabsValue
         }
-        let distance = highValue - lowValue
+        let distance = (highValue - lowValue).roundToSix()
         return distance
     } else {
         if startPoint.sign == false && targetPoint.sign == true {
             let distance = startPoint.fabsValue + targetPoint.fabsValue
             return distance
         } else if startPoint.sign == true && targetPoint.sign == false {
-            let distance = startPoint.fabsValue + targetPoint.fabsValue
+            let distance = (startPoint.fabsValue + targetPoint.fabsValue).roundToSix()
             return distance
         } else {
             var highValue: Double = startPoint.fabsValue
@@ -50,36 +58,74 @@ func distancePointToPoint(_ startPoint: PointInfoStruct, _ targetPoint: PointInf
                 highValue = targetPoint.fabsValue
                 lowValue = startPoint.fabsValue
             }
-            let distance = highValue - lowValue
+            let distance = (highValue - lowValue).roundToSix()
             return distance
         }
     }
 }
-
 func distanceCenterPoint(_ centerPoint: PointInfoStruct,
-                         andOtherPoints points: [PointInfoStruct]) -> [Double: ResultStruct] {
-    var resultDic: [Double: ResultStruct] = [Double: ResultStruct]()
+                         andOtherPoints points: [PointInfoStruct]) -> ResultStruct? {
+    var lowValue: ResultStruct?
     for point in points {
-        let distance = distancePointToPoint(centerPoint, point)
-        var centerPointString = "\(centerPoint.sign == true ? "":"-" )\(centerPoint.fabsValue)"
-        if centerPointString == "-0.0" {
-            centerPointString = "0.0"
+        if centerPoint.sign != point.sign || centerPoint.fabsValue != point.fabsValue {
+            let distance = distancePointToPoint(centerPoint, point)
+            let result = ResultStruct(centerPoint: centerPoint, anotherPoint: point, distance: distance)
+            if lowValue == nil {
+                lowValue = result
+            } else {
+                let compareValue = (lowValue?.distance)! > result.distance ? result: lowValue
+                lowValue = compareValue
+            }
         }
-        let anotherPointString = "\(point.sign == true ? "":"-" )\(point.fabsValue)"
-        let result = ResultStruct(centerPointString: centerPointString, anotherPointString: anotherPointString, distance: distance)
-        resultDic[distance] = result
     }
-    return resultDic
+    return lowValue
+}
+func getResults(_ points: [PointInfoStruct]) -> [ResultStruct] {
+    var standardPoints = points
+    standardPoints.removeLast()
+    var targetPoints = points
+    targetPoints.removeFirst()
+    var results: [ResultStruct] = [ResultStruct]()
+    for standardPoint in standardPoints {
+        let result = distanceCenterPoint(standardPoint, andOtherPoints: targetPoints)
+        targetPoints.removeFirst()
+        results.append(result!)
+    }
+    return results
 }
 
-func whatIsNearPoint(_ results: [Double: ResultStruct]) {
-    var keyValues: [Double] = [Double]()
-    for result in results {
-        keyValues.append(result.key)
+//// 여기서 원하는 숫자를 넣으면 됨
+let points = [0, -9, -3.67, 5.78, 10.1, 9.0, -1.1, -2.2]
+////
+if points.count > 2 {
+    let pointArray: [PointInfoStruct] =  makePoints(points)
+    let results = getResults(pointArray)
+    var sortedResults = results.sorted { (first, second) -> Bool in
+        first.distance < second.distance
     }
-    let minValue = keyValues.min()
-    print("기준점: \(results[minValue!]?.centerPointString ?? "오류"),\n가장 가까운점: \(results[minValue!]?.anotherPointString ?? "오류")")
+    let lowResult = sortedResults.removeFirst()
+    var equalDistanceResult: [ResultStruct] = [ResultStruct]()
+    for result in sortedResults {
+        if lowResult.distance == result.distance {
+            equalDistanceResult.append(result)
+        }
+    }
+    if equalDistanceResult.count == 0 {
+        let centerPoint = "\(lowResult.centerPoint.sign == true ? "" : "-")" + "\(lowResult.centerPoint.fabsValue)"
+        let anotherPoint = "\(lowResult.anotherPoint.sign == true ? "" : "-")" + "\(lowResult.anotherPoint.fabsValue)"
+        print("가장 가까운 점은 \(centerPoint)와 \(anotherPoint)로 거리는 \(lowResult.distance)이다")
+    } else {
+        print("동일한 거리를 가지는 가까운 점이 \(equalDistanceResult.count + 1)개 있다.\n")
+        let centerPoint = "\(lowResult.centerPoint.sign == true ? "" : "-")" + "\(lowResult.centerPoint.fabsValue)"
+        let anotherPoint = "\(lowResult.anotherPoint.sign == true ? "" : "-")" + "\(lowResult.anotherPoint.fabsValue)"
+        print("가장 가까운 점은 \(centerPoint)와 \(anotherPoint)로 거리는 \(lowResult.distance)이다.\n")
+        for result in equalDistanceResult {
+            let resultCPoint = "\(result.centerPoint.sign == true ? "" : "-")" + "\(result.centerPoint.fabsValue)"
+            let resultAPoint = "\(result.anotherPoint.sign == true ? "" : "-")" + "\(result.anotherPoint.fabsValue)"
+            print("가장 가까운 점은 \(resultCPoint)와 \(resultAPoint)로 거리는 \(result.distance)이다.\n")
+        }
+    }
+} else {
+    print("3개 이상의 숫자를 넣어주세요.")
 }
 
-let resluts = distanceCenterPoint(centerPoint, andOtherPoints: pointArray)
-whatIsNearPoint(resluts)
