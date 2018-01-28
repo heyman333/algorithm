7b74b63c8d9833b7689b4eee0d420739699af3d3
youngsu_Han <gglife333@gmail.com>
2018-01-29 02:18:47 +0900

    Update youngsoo.swift

--- 1st_problem/youngsoo.swift
+++ 1st_problem/youngsoo.swift
@@ -1,7 +1,6 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
 func minAbstraction(_ arr:[Int]) {
     
     var abstractionArr:[Int] = []
@@ -12,16 +11,16 @@ func minAbstraction(_ arr:[Int]) {
             abstractionArr.append(abs)
         }
     }
+    
     let sortedArr = abstractionArr.sorted {$0 < $1}
+    
     for (index, value) in abstractionArr.enumerated() {
         if (sortedArr[0] == value) {
             print("\(index + 1)번째 \(index + 2)번째 숫자")
             print(arr[index], arr[index+1])
         }
     }
-    
 }
-
 var nums = [1, 20, 21, 40, 50, 51, 52] // 정렬된 배열
 minAbstraction(nums)
 
