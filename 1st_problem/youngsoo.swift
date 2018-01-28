572931d221610046b9a5750856106e6d74867f8d
youngsu_Han <gglife333@gmail.com>
2018-01-29 02:23:02 +0900

    edit typo

--- 1st_problem/youngsoo.swift
+++ 1st_problem/youngsoo.swift
@@ -1,20 +1,20 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-func minAbstraction(_ arr:[Int]) {
+func minSubtraction(_ arr:[Int]) {
     
-    var abstractionArr:[Int] = []
+    var subtractionArr:[Int] = []
     
     for (index, _) in arr.enumerated() {
         if (index != arr.count - 1) {
             let abs = arr[index+1] - arr[index]
-            abstractionArr.append(abs)
+            subtractionArr.append(abs)
         }
     }
     
-    let sortedArr = abstractionArr.sorted {$0 < $1}
+    let sortedArr = subtractionArr.sorted {$0 < $1}
     
-    for (index, value) in abstractionArr.enumerated() {
+    for (index, value) in subtractionArr.enumerated() {
         if (sortedArr[0] == value) {
             print("\(index + 1)번째 \(index + 2)번째 숫자")
             print(arr[index], arr[index+1])
@@ -22,7 +22,7 @@ func minAbstraction(_ arr:[Int]) {
     }
 }
 var nums = [1, 20, 21, 40, 50, 51, 52] // 정렬된 배열
-minAbstraction(nums)
+minSubtraction(nums)
 
 
 
